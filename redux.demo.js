42034963dca78ced8bc7901477c79d4519a021b2
yashdobariya <yashdobariya888@gmail.com>
2021-09-18 14:41:19 +0530

    basic redux

--- redux.demo.js
+++ redux.demo.js
@@ -1,9 +1,21 @@
 const redux = require('redux');
 
-const counterReducer = (state = { sum: 5 }, action) => {
-    return {
-        sum: state.sum + 1
+const counterReducer = (state = { sum: 0 }, action) => {
+
+    if (action.type === 'increment') {
+        return {
+            sum: state.sum + 1
+        }
     }
+
+    if (action.type === 'decrement') {
+        return {
+            sum: state.sum - 1
+        }
+    }
+
+    return state;
+
 }
 
 const store = redux.createStore(counterReducer);
@@ -16,3 +28,4 @@ const counterSubscriber = () => {
 store.subscribe(counterSubscriber)
 
 store.dispatch({ type: 'increment' })
+store.dispatch({ type: 'decrement' })
